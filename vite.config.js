d53eb9960edc5665dce66f966192217c629e86cf
laibatariq110 <laibatariq110@gmail.com>
2024-12-10 14:47:52 +0500

    fixing error

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/portfolio/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
 });
